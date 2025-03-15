bc617a6ac0e802736c5a6e71f5374050f9bf35a6
smash07-batman <anubhawpandey99@gmail.com>
2025-03-15 15:48:19 +0530

    First Commint

---
+++ Section_01/Javascript_folder/function.js
@@ -0,0 +1,18 @@
+function addNums(a,b){
+  var  c=a+b;
+    console.log(c);
+    
+}
+
+//
+addNums (10,20);
+
+//console.log(c);
+
+const getAvg = function (m1, m2 , m3){
+    let avg = (m1+ m2+ m3) / 3;
+    return avg;
+}
+
+const result = getAvg (20,20,20)
+console.log(result);
